0092cdd39aad77c02c63147ed88304eaf710e166
RoderickJDunn <roderickjdunn@gmail.com>
2020-06-10 19:48:40 -0400

    Version 1.2.4

--- dl_links.js
+++ dl_links.js
@@ -1,2 +1,2 @@
-let iosLink = "https://i.diawi.com/2QoPDp";
-let androidLink = "https://i.diawi.com/3K5T6s";
+let iosLink = "https://i.diawi.com/4Udu5t";
+let androidLink = "https://i.diawi.com/gJnve9";
